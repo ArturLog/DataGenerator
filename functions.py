47a5c0f4a3848e59c5cf3160cb510298b7fffdbc
Artur <artur.spiewak2967@gmail.com>
2023-10-31 16:32:51 +0100

    Add create schema function

--- functions.py
+++ functions.py
@@ -39,3 +39,48 @@ def generate_random_time():
     time = f"{hour:02d}:{minute:02d}"
     
     return time
+
+def create_schema():
+  with open('schema.sql', 'w', encoding='utf-8') as file:
+    file.write('''
+  CREATE DATABASE Baza;
+  
+  CREATE TABLE Uzytkownik (
+      PESEL INTEGER PRIMARY KEY,
+      Imie TEXT,
+      Nazwisko TEXT,
+      Plec TEXT,
+      Data_urodzenia DATE,
+      Data_uzyskania_prawa_jazdy DATE
+  );
+   
+  CREATE TABLE Przejazd (
+      ID INTEGER primary key,
+      PESEL_Uzytkownika INTEGER,
+      Nr_rejestracyjny_pojazdu TEXT,
+      Data_rozpoczecia DATE,
+      Godzina_rozpoczecia DATE,
+      Data_zakonczenia DATE,
+      Godzina_zakonczenia DATE,
+      Miejsce_rozpoczecia INTEGER,
+      Miejsce_zakonczenia INTEGER,
+      Dystans INTEGER,
+      Ocena_przejazdu FLOAT,
+      Ocena_techniki_jazdy FLOAT,
+      Ocena_przestrzegania_przepisow_drogowych FLOAT,
+      Mnoznik_ceny FLOAT,
+      Koszt_przejazdu FLOAT,
+      FOREIGN KEY (PESEL_Uzytkownika) REFERENCES Uzytkownik(PESEL)
+  );
+
+  CREATE TABLE Bledy_przejazdu (
+      ID INTEGER PRIMARY KEY,
+      ID_Przejazdu INTEGER,
+      Data DATE,
+      Godzina DATE,
+      Miejsce TEXT,
+      Typ TEXT,
+      Odchyl_od_poprawnej_wartosci INTEGER,
+      FOREIGN KEY (ID_Przejazdu) REFERENCES Przejazd(ID)
+  );
+            ''')
--- main.py
+++ main.py
@@ -1,14 +1,19 @@
 from datetime import datetime
 from services.Generator import Generator
+from functions import create_schema
 
 AMOUNT_OF_USERS_FIRST_PERIOD = 1000
 AMOUNT_OF_USERS_SECOND_PERIOD = 500
+
 AMOUNT_OF_RIDES_FIRST_PERIOD = 1000
 AMOUNT_OF_RIDES_SECOND_PERIOD = 500
+
 AMOUNT_OF_MISTAKES_FIRST_PERIOD = 1000
 AMOUNT_OF_MISTAKES_SECOND_PERIOD = 500
+
 AMOUNT_OF_CARS_FIRST_PERIOD = 1000
 AMOUNT_OF_CARS_SECOND_PERIOD = 500
+
 AMOUNT_OF_LOCATIONS_FIRST_PERIOD = 1000
 AMOUNT_OF_LOCATIONS_SECOND_PERIOD = 500
 
@@ -17,6 +22,7 @@ T1 = datetime(2001, 6, 6, 0, 0, 1)
 T2 = datetime(2002, 1, 1, 0, 0, 1)
 
 if __name__ == '__main__':
+    create_schema()
     generator = Generator()
     generator.config(start_date=T0, end_date=T1,
                           users_amount=AMOUNT_OF_USERS_FIRST_PERIOD,
